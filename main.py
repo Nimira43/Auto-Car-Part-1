460f6efab084d0be392a4e89964f00e18db94500
Nimira43 <n.rabone72@aol.com>
2024-08-18 22:21:38 +0100

    Imported image, plus other images, import cv2, loaded image.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello')
+import cv2
+
+image = cv2.imread('./images/test-1.jpg')
+
+# My Images 
+
+# image = cv2.imread('./images/test-2.jpg')
+# image = cv2.imread('./images/test-3.jpg')
+# image = cv2.imread('./images/test-4.jpg')
+# image = cv2.imread('./images/test-5.jpg')
+# image = cv2.imread('./images/test-6.jpg')
+
+
+cv2.imshow('result', image)
+cv2.waitKey(0)
