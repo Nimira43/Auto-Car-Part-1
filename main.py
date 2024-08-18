afa488df7d01759cb939bb77dcba99f43cd8e2ef
Nimira43 <n.rabone72@aol.com>
2024-08-18 23:17:16 +0100

    Smoothening Images - reduce noise using Gaussian Blur by filtering it out

--- main.py
+++ main.py
@@ -12,12 +12,9 @@ image = cv2.imread('./images/test-6.jpg')
 
 lane_image = np.copy(image)
 grey = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-cv2.imshow('result', grey)
+blur = cv2.GaussianBlur(grey, (5, 5), 0)
+
+cv2.imshow('result', blur)
 cv2.waitKey(0)
 
-# Notes -
-# test-1 image is provide by instructor, the rest by me
-# test-4 and test-6 image look useful to look at further
-# not sure about test-5, test-3, test-2 images - vehicles in image
-
-# Edge Detection - GrayScale Conversion - images saved 
+# Smoothening Images - reduce noise using Gaussian Blur by filtering it out 
