15bfe4b8d511a14ec204950c1a1497d6d4838624
Nimira43 <n.rabone72@aol.com>
2024-08-21 20:21:20 +0100

    using cv2.Canny to detect simple edge detection. Will carry on with Test 1, Test4 and Test 6 afterwards. The lines detected were very high in Tests 2, 3 and 5.

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import cv2
 import numpy as np
 
-# image = cv2.imread('./images/test-1.jpg')
+image = cv2.imread('./images/test-1.jpg')
 
 # My Images 
-# image = cv2.imread('./images/test-2.jpg')
-# image = cv2.imread('./images/test-3.jpg')
+
 # image = cv2.imread('./images/test-4.jpg')
-# image = cv2.imread('./images/test-5.jpg')
-image = cv2.imread('./images/test-6.jpg')
+# image = cv2.imread('./images/test-6.jpg')
 
 lane_image = np.copy(image)
 grey = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
 blur = cv2.GaussianBlur(grey, (5, 5), 0)
+canny = cv2.Canny(blur, 50, 150)
 
-cv2.imshow('result', blur)
+cv2.imshow('result', canny)
 cv2.waitKey(0)
