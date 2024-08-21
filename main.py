d53767489072c73508c954f99f218343a2256070
Nimira43 <n.rabone72@aol.com>
2024-08-21 20:29:10 +0100

    Code refactored.

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import cv2
 import numpy as np
 
+def canny(image):
+  grey = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+  blur = cv2.GaussianBlur(grey, (5, 5), 0)
+  canny = cv2.Canny(blur, 50, 150)
+  return canny
+
+
 image = cv2.imread('./images/test-1.jpg')
 
 # My Images 
-
 # image = cv2.imread('./images/test-4.jpg')
 # image = cv2.imread('./images/test-6.jpg')
 
 lane_image = np.copy(image)
-grey = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-blur = cv2.GaussianBlur(grey, (5, 5), 0)
-canny = cv2.Canny(blur, 50, 150)
+canny = canny(lane_image)
 
 cv2.imshow('result', canny)
 cv2.waitKey(0)
